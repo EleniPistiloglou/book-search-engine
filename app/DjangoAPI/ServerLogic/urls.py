78fb4126272189fe168aa5e3ea375d420b1206ef
EleniPistiloglou <72700267+EleniPistiloglou@users.noreply.github.com>
2022-04-12 03:05:17 +0200

    Update urls.py

--- app/DjangoAPI/ServerLogic/urls.py
+++ app/DjangoAPI/ServerLogic/urls.py
@@ -3,6 +3,7 @@ from ServerLogic import views
 
 
 urlpatterns=[
-    re_path(r'^basic/([a-zA-Z0-9]+)$', views.getBasicAPI), 
-    re_path(r'^advanced/(.+)$', views.getAdvancedAPI), 
+    re_path(r'^basic/([a-zA-Z0-9]*)$', views.getBasicAPI), 
+    re_path(r'^advanced/(.*)$', views.getAdvancedAPI), 
+    re_path(r'^recomendations$', views.getRecomendations)
 ]
